7bb87dbffa6b6aaa23cef15efc1830f974b4d053
coco24 <1281299809@qq.com>
2022-09-16 12:28:22 +0800

    Initial commit

---
+++ cl-gtk4.asd
@@ -0,0 +1,34 @@
+#+sb-core-compression
+(defmethod asdf:perform ((o asdf:image-op) (c asdf:system))
+  (uiop:dump-image (asdf:output-file o c) :executable t :compression t))
+
+(defsystem cl-gtk4
+  :version "1.0.0"
+  :author "Bohong Huang <1281299809@qq.com>"
+  :maintainer "Bohong Huang <1281299809@qq.com>"
+  :license "lgpl3"
+  :description "GTK4 binding for Common Lisp."
+  :homepage "https://github.com/BohongHuang/cl-gtk4"
+  :bug-tracker "https://github.com/BohongHuang/cl-gtk4/issues"
+  :source-control (:git "https://github.com/BohongHuang/cl-gtk4.git")
+  :serial t
+  :components ((:file "gtk4"))
+  :depends-on (#:cl-gobject-introspection-wrapper))
+
+(uiop:register-image-restore-hook
+ (lambda ()
+   (let* ((namespace "Gtk")
+          (package (find-package (string-upcase namespace))))
+     (when package
+       (setf (symbol-value (find-symbol "*NS*" package))
+             (uiop:symbol-call :gir :require-namespace namespace "4.0"))))))
+
+(defsystem cl-gtk4/example
+  :depends-on (#:asdf
+               #:cl-gtk4
+               #:cl-gio)
+  :build-operation program-op
+  :build-pathname "cl-gtk4-example"
+  :entry-point "gtk4.example:main"
+  :pathname "./example"
+  :components ((:file "gtk4")))
---
+++ gtk4.lisp
@@ -0,0 +1,36 @@
+;;;; gtk4.lisp
+
+;;;; Copyright (C) 2022 Bohong Huang
+;;;;
+;;;; This program is free software: you can redistribute it and/or modify
+;;;; it under the terms of the GNU Lesser General Public License as published by
+;;;; the Free Software Foundation, either version 3 of the License, or
+;;;; (at your option) any later version.
+;;;;
+;;;; This program is distributed in the hope that it will be useful,
+;;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;;; GNU Lesser General Public License for more details.
+;;;;
+;;;; You should have received a copy of the GNU Lesser General Public License
+;;;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+(cl:defpackage gtk4
+  (:nicknames #:gtk)
+  (:export #:*ns* #:connect))
+
+(cl:in-package #:gtk4)
+
+(cl:eval-when (:execute :compile-toplevel :load-toplevel)
+  (cl:setf gir-wrapper:*quoted-name-alist* '(("ATContext" . at-context)
+                                             (("TextBuffer" . "get_insert") . text-buffer-get-insert)
+                                             (("Gesture" . "group") . group-gestures)
+                                             (("Widget" . "is_sensitive") . widget-is-sensitive-p)
+                                             (("Widget" . "is_visible") . widget-is-visible-p))))
+
+(gir-wrapper:define-gir-namespace "Gtk" "4.0")
+
+(cl:setf (cl:fdefinition 'connect) #'gir:connect)
+
+(cl:eval-when (:execute :compile-toplevel :load-toplevel)
+  (cl:setf gir-wrapper:*quoted-name-alist* cl:nil))
