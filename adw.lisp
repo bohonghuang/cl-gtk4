2cce9dfc8a2649966b7ee1122a7b84ece19ce597
coco24 <1281299809@qq.com>
2022-09-18 19:41:07 +0800

    Add an empty `:use` clause to `defpackage`

--- adw.lisp
+++ adw.lisp
@@ -15,7 +15,9 @@
 ;;;; You should have received a copy of the GNU Lesser General Public License
 ;;;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-(cl:defpackage adw (:export #:*ns* #:connect))
+(cl:defpackage adw
+  (:use)
+  (:export #:*ns*))
 
 (cl:in-package #:adw)
 
