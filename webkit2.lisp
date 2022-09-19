1ad24b3fb90e004cac048039ed7e8f963c5340ee
coco24 <1281299809@qq.com>
2022-09-19 11:47:11 +0800

    Add WebKit2GTK binding

---
+++ webkit2.lisp
@@ -0,0 +1,25 @@
+;;;; webkit2.lisp
+
+;;;; Copyright (C) 2022 Bohong Huang
+;;;;
+;;;; This program is free software: you can redistribute it and/or modify
+;;;; it under the terms of the GNU Lesser General Public License as published by
+;;;; the Free Software Foundation, either version 3 of the License, or
+;;;; (at your option) any later version.
+;;;;
+;;;; This program is distributed in the hope that it will be useful,
+;;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;;; GNU Lesser General Public License for more details.
+;;;;
+;;;; You should have received a copy of the GNU Lesser General Public License
+;;;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+(cl:defpackage webkit2
+  (:use)
+  (:nicknames #:webkit)
+  (:export #:*ns*))
+
+(cl:in-package #:webkit2)
+
+(gir-wrapper:define-gir-namespace "WebKit2" "5.0")
